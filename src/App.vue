69b8f82ec754d6d754d000ffee2e3583f75787a3
Lars Eichler <lars.eichler@turbinekreuzberg.com>
2026-03-13 09:38:15 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import '@carbon/web-components/es/components/checkbox/checkbox.js';
+</script>
+
+<template>
+  <main>
+    <cds-checkbox helper-text="Helper text goes here">Checkbox label</cds-checkbox>
+    <br><br>
+    <cds-checkbox invalid="" invalid-text="Invalid test goes here">Checkbox label</cds-checkbox>
+    <br><br>
+    <cds-checkbox warn="" warn-text="Warning test goes here">Checkbox label</cds-checkbox>
+    <br><br>
+    <cds-checkbox readonly="">Checkbox label</cds-checkbox>
+  </main>
+</template>
+
